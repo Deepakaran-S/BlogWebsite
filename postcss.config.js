73d552ce26ae92b7db009d50b54b1e74366b125a
MADDY <pgtvmaddy@gmail.com>
2025-08-06 12:58:33 +0530

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: c:\Users\M A D D Y\BlogWebsite\postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: c:\Users\M A D D Y\BlogWebsite\tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
