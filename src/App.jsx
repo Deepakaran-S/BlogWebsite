a70a1cd11dc7e72aa478218d53b6dbcb8ee66881
MADDY <pgtvmaddy@gmail.com>
2025-08-08 11:05:57 +0530

    4rth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Layout from './layout';
 import Header from './Pages/Header';
-import Footer from './Pages/Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Layout />
-      <Footer />
+      
     </div>
 
   );
--- src/layout.jsx
+++ src/layout.jsx
@@ -3,10 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-import Dashboard from "./Pages/dashboard";
 import Create from "./Pages/Create";
 import EditBlog from "./Pages/EditBlog";
 import BlogDetail from "./Pages/BlogDetail";
+import Dashboard from "./Pages/Blogpage";
 
 
 const Layout = () => {
